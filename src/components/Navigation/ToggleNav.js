1d43e91bd7b079a6839f6b7d75f61318d358ba7c
Nima <nima.73sm@yahoo.com>
2021-11-16 21:56:18 +0330

    make some reforms for responsive design

--- src/components/Navigation/ToggleNav.js
+++ src/components/Navigation/ToggleNav.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useState } from "react";
 import classes from "./ToggleNav.module.css";
 import menuAdd from "./../../assets/svg/menu-add-line.svg";
 import menu from "./../../assets/svg/menu-line.svg";
@@ -10,6 +10,10 @@ const ToggleNav = (props) => {
     setClicked((clicked) => !clicked);
   };
 
+  const closeNavHandler = () => {
+    setClicked(false);
+  };
+
   const navExpandedClasses = !clicked
     ? `${classes.nav_expanded} ${classes.navClose}`
     : `${classes.nav_expanded} ${classes.navOpen}`;
@@ -17,27 +21,25 @@ const ToggleNav = (props) => {
   return (
     <>
       <nav className={classes.nav}>
-        <div className={classes.nav_bar}>
-          <button onClick={navClickHandler}>
-            {!clicked && <img src={menuAdd} alt="open-tab" />}
-            {clicked && <img src={menu} alt="close-tab" />}
-          </button>
-          <h2 onClick={props.goToIntro}>
-            N<span>i</span>ma Mogh<span>a</span>dd<span>a</span>m
-          </h2>
+        <button onClick={navClickHandler}>
+          {!clicked && <img src={menuAdd} alt="open-tab" />}
+          {clicked && <img src={menu} alt="close-tab" />}
+        </button>
+        <h2 onClick={props.goToIntro}>
+          N<span>i</span>ma Mogh<span>a</span>dd<span>a</span>m
+        </h2>
+        <div className={navExpandedClasses} onMouseLeave={closeNavHandler}>
+          <div className={classes.nav_expanded__slide}>
+            <ul>
+              <li onClick={props.goToIntro}>درباره من</li>
+              <li onClick={props.goToServices}>خدمات من</li>
+              <li onClick={props.goToSkills}>مهارت ها</li>
+              <li onClick={props.goToBlog}>بلاگ</li>
+              <li onClick={props.goToContact}>ارتباط با من</li>
+            </ul>
+          </div>
         </div>
       </nav>
-      <div className={navExpandedClasses}>
-        <div className={classes.nav_expanded__slide}>
-          <ul>
-            <li onClick={props.goToIntro}>درباره من</li>
-            <li onClick={props.goToServices}>خدمات من</li>
-            <li onClick={props.goToSkills}>مهارت ها</li>
-            <li onClick={props.goToBlog}>بلاگ</li>
-            <li onClick={props.goToContact}>ارتباط با من</li>
-          </ul>
-        </div>
-      </div>
     </>
   );
 };
